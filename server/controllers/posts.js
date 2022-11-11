576afcc27fd04564271695949b8a1d7402456770
Christy <105733880+cmkc109@users.noreply.github.com>
2022-11-11 16:55:47 -0500

    blog

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -5,7 +5,6 @@ import PostMessage from '../models/postMessage.js'
 export const getPosts = async (req, res) => {
     try {
         const postMessages = await PostMessage.find();
-        console.log(postMessages);
         res.status(200).json(postMessages)
         
     } catch(error) {
